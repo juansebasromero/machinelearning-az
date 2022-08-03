4aa4e1e3b8e435e76f03e8b7beb490bd1c15317d
juansebasromero <107513088+juansebasromero@users.noreply.github.com>
2022-08-03 11:32:19 -0500

    Update multiple_linear_regression.R

--- datasets/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
+++ datasets/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
@@ -47,3 +47,22 @@ summary(regression)
 regression = lm(formula = Profit ~ R.D.Spend,
                 data = dataset)
 summary(regression)
+
+# Eliminación Automática hacia atrás
+backwardElimination <- function(x, sl) {
+  numVars = length(x)
+  for (i in c(1:numVars)){
+    regressor = lm(formula = Profit ~ ., data = x)
+    maxVar = max(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"])
+    if (maxVar > sl){
+      j = which(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"] == maxVar)
+      x = x[, -j]
+    }
+    numVars = numVars - 1
+  }
+  return(summary(regressor))
+}
+ 
+SL = 0.05
+dataset = dataset[, c(1,2,3,4,5)]
+backwardElimination(training_set, SL)
